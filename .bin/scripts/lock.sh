bdcd828449e3ec05c33510c6b49ba83854e4ee31
Michael Gold <goldmichael@gmail.com>
2017-09-20 12:37:07 -0400

    added tnp

--- .bin/scripts/lock.sh
+++ .bin/scripts/lock.sh
@@ -14,7 +14,7 @@ convert $TEMPDIR/head_0.png -scale 5% -scale 2000% $TEMPDIR/head_0.png
 if [ -e $TEMPDIR/head_1.png ]; then
   convert $TEMPDIR/head_1.png -scale 5% -scale 2000% $TEMPDIR/head_1.png
   [[ -f $HOME/Pictures/misc/lock.png ]] && convert $TEMPDIR/head_1.png $HOME/Pictures/misc/lock.png -gravity center -composite -matte $TEMPDIR/head_1.png
-  convert $TEMPDIR/head_1.png $TEMPDIR/head_0.png -append $TEMPDIR/screen.png
+  convert $TEMPDIR/head_1.png $TEMPDIR/head_0.png +append $TEMPDIR/screen.png
   rm $TEMPDIR/head_1.png
 else
   mv $TEMPDIR/head_0.png $TEMPDIR/screen.png
