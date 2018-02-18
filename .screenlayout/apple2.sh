c387c2412f28d62d01fab7e9f86c75a4d5bab4fc
Michael Gold <goldmichael@gmail.com>
2018-02-18 10:01:41 -0500

    commented out the command to change desktop wallpaper

--- .screenlayout/apple2.sh
+++ .screenlayout/apple2.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 xrandr --output HDMI-2 --off --output HDMI-1 --off --output eDP1 --primary --mode 1920x1080 --pos 2560x0 --rotate normal --output DP2 --mode 2560x1440 --pos 0x0 --rotate normal
-feh -Z --bg-fill /usr/share/backgrounds/Passion_by_Vilia_Majere.jpg  
+# feh -Z --bg-fill /usr/share/backgrounds/Passion_by_Vilia_Majere.jpg  
