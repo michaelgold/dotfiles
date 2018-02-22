4c7126a552082bb5f08fe610483660606be91d94
Michael Gold <goldmichael@gmail.com>
2018-02-22 16:33:39 -0500

    new screen layout files

---
+++ .screenlayout/apple3.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 2560x360 --rotate normal --output DP1 --mode 1920x1080 --pos 4480x0 --rotate left --output HDMI2 --off --output HDMI1 --off --output DP2 --mode 2560x1440 --pos 0x0 --rotate normal
---
+++ .screenlayout/hdmivert.sh
@@ -0,0 +1,4 @@
+# enable multiple monitors on HiDPI 
+exec --no-startup-id /usr/bin/xrandr --output HDMI-1 --off --output DP-1 --pos 0x0 --output eDP-1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output DP-2 --pos 0x0 --rotate normal 
+ 
+
